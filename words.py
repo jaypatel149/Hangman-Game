b73d80eff9f198a30fdf717184e83829568c39df
Jay patel <46785231+jaypatel149@users.noreply.github.com>
2022-09-07 22:26:56 +0530

    Update words.py
    change code

--- words.py
+++ words.py
@@ -1,24 +1,14 @@
-# import string
-# import random
+import random
 
-# def load_words():
+def load_words():
+    with open("words.txt", 'r+') as file:
+         mylist = [line.rstrip('\n').split(" ") for line in file]
+    return mylist[0]
 
-#     WORDLIST_FILENAME = "words.txt"
-#     inFile = open(WORDLIST_FILENAME, 'r')
-#     line = inFile.readline()
-#     word_list = string.split(line)
+def choose_word():
+    word_list = load_words()
+    secret_word = random.choice(word_list)
+    secret_word = secret_word.lower()
 
-#     return word_list
-#     print("word_list")
-
-# def choose_word():
-#     """
-#     word_list (list): list of words (strings)
-#     ye function ek word randomly return karega
-#     """
-#     word_list = load_words()
-#     secret_word = random.choice(word_list)
-#     secret_word = secret_word.lower()
-
-#     return secret_word
-#     print("secret_word")
+    return secret_word
+print(choose_word())
